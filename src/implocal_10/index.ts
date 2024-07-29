4cdc1f311feda07956a93578f515c5bc77437caa
Fernando Isidro Luna <luffynando@gmail.com>
2024-07-28 21:07:51 -0600

    build: :construction: include exports for implocal10 elements and utilities sumas conceptos

---
+++ src/implocal_10/index.ts
@@ -0,0 +1,3 @@
+export { default as ImpuestosLocales } from '#src/implocal_10/impuestos_locales';
+export { default as RetencionesLocales } from '#src/implocal_10/retenciones_locales';
+export { default as TrasladosLocales } from '#src/implocal_10/traslados_locales';
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 export { default as AbstractElement } from '#src/common/abstract_element';
+export { default as SumasConceptos } from '#src/utilities/sumas_conceptos';
+export { default as SumasConceptosWriter } from '#src/utilities/sumas_conceptos_writer';
