9fb51401264ba4ed074bc32f66fecb58328e8f14
Fernando Isidro Luna <luffynando@gmail.com>
2024-08-16 14:04:33 -0600

    chore: update dependencies and add support to handle peer dependencie cfdi-core

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,9 @@ import { nodecfdiConfig } from '@nodecfdi/eslint-config';
 import { defineFlatConfig } from 'eslint-define-config';
 
 export default defineFlatConfig([
-  ...nodecfdiConfig({ vitest: true, ignores: { additional: ['**/*.stub', 'ace.js'] } }),
-  {
-    files: ['**/*.test.ts', '**/*.spec.ts', '**/tests/**/*'],
-    rules: {
-      'vitest/no-done-callback': 'off',
-    },
-  },
+  ...nodecfdiConfig({
+    vitest: true,
+    experimentalProjectService: import.meta.dirname,
+    ignores: { additional: ['**/*.stub'] },
+  }),
 ]);
