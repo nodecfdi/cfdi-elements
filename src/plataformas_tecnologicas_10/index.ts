cd61a0b801c795c36cf88a5b4c8d9773f7bee0c5
Fernando Isidro Luna <luffynando@gmail.com>
2024-09-10 23:21:16 -0600

    build: :sparkles: add new export plataformasTecnologicas10

---
+++ src/plataformas_tecnologicas_10/index.ts
@@ -0,0 +1,6 @@
+export { default as ComisionDelServicio } from '#src/plataformas_tecnologicas_10/comision_del_servicio';
+export { default as ContribucionGubernamental } from '#src/plataformas_tecnologicas_10/contribucion_gubernamental';
+export { default as DetallesDelServicio } from '#src/plataformas_tecnologicas_10/detalles_del_servicio';
+export { default as ImpuestosTrasladadosdelServicio } from '#src/plataformas_tecnologicas_10/impuestos_trasladadosdel_servicio';
+export { default as Servicios } from '#src/plataformas_tecnologicas_10/servicios';
+export { default as ServiciosPlataformasTecnologicas } from '#src/plataformas_tecnologicas_10/servicios_plataformas_tecnologicas';
