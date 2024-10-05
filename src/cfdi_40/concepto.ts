df6f26c2b94740ed8dc0dd9b379803ec141a4628
Fernando Isidro Luna <luffynando@gmail.com>
2024-10-04 21:47:21 -0600

    refactor: :recycle: prefer usage of alias import in package.json

--- src/cfdi_40/concepto.ts
+++ src/cfdi_40/concepto.ts
@@ -2,12 +2,12 @@ import { type XmlNodeInterface } from '@nodecfdi/cfdi-core/types';
 import { Mixin } from 'ts-mixer';
 import ACuentaTerceros from '#src/cfdi_40/a_cuenta_terceros';
 import ComplementoConcepto from '#src/cfdi_40/complemento_concepto';
+import ConceptoImpuestos from '#src/cfdi_40/concepto_impuestos';
 import CuentaPredial from '#src/cfdi_40/cuenta_predial';
 import InformacionAduanera from '#src/cfdi_40/informacion_aduanera';
 import WithImpuestos from '#src/cfdi_40/mixins/with_impuestos';
 import Parte from '#src/cfdi_40/parte';
 import AbstractElement from '#src/common/abstract_element';
-import ConceptoImpuestos from './concepto_impuestos.js';
 
 export default class Concepto extends Mixin(AbstractElement, WithImpuestos) {
   public getElementName(): string {
