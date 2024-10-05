7179aa0e6154ee74815864477e9d70c57c43a329
Fernando Isidro Luna <luffynando@gmail.com>
2024-10-04 21:48:02 -0600

    chore: :arrow_up: update dependencies and update configs for latest changes in org

--- eslint.config.js
+++ eslint.config.js
@@ -4,11 +4,3 @@ import nodecfdiConfig from '@nodecfdi/eslint-config';
 const { defineConfig } = nodecfdiConfig(import.meta.dirname, { vitest: true });
 
 export default defineConfig();
-
-// export default defineFlatConfig([
-//   ...nodecfdiConfig({
-//     vitest: true,
-//     experimentalProjectService: import.meta.dirname,
-//     ignores: { additional: ['**/*.stub'] },
-//   }),
-// ]);
